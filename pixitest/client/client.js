2bc60f9965846891cae1086d4dab12f612f847d2
TeddyTsui <teddy_tsui@163.com>
2018-11-27 18:22:19 +0800

    update some code

--- pixitest/client/client.js
+++ pixitest/client/client.js
@@ -2,6 +2,7 @@ import { keyboard } from './utils.js'
 import { createSprite, playAnim } from './charactor.js'
 import { doSimpleLogic } from './movement.js'
 
+const defaultDelay = 10
 
 const response = true //simulation
 const dataTemplate = {
@@ -16,6 +17,43 @@ const playerTemplate = {
         HP: '100/100'
     }
 }
+
+const initStatus = {
+    // position
+    x: 0,
+    y: 0,
+    z: 0,
+
+    // speed
+    vx: 0,
+    vy: 0,
+    vz: 0,
+    ax: 0,
+    ay: 0,
+    az: .6,
+
+    // action
+    lastX: 'right',
+    lastY: 'down',
+    motion: 'right_idle',
+    jumping: false,
+    actionCounter: 0,
+    attackCounter: 0,
+    dashCounter: 0,
+
+    // ability
+    speedDown: .4,
+    actionCoolDown: 10,
+    attackCoolDown: 20,
+    dashCoolDown: 10,
+
+    //status
+    HP: 100,
+    maxHP: 100,
+    MP: 60,
+    maxMP: 60
+}
+
 
 const controllConfig = {
     /**
@@ -44,6 +82,7 @@ let Application = PIXI.Application,
 let players = [] //local sprites
 
 let app = undefined,
+    socket,
     keys = {},
     state
 
@@ -51,8 +90,15 @@ let gameFrame,
     nextGameFrame,
     maxFrameCache = 60 * 60 * 60,
     fpsRadio = 2,
-    updateCache = [],
-    logicFrameCahce = []
+    localPlayerId,
+    updateControlInterval
+    // updateCache = [],
+    // logicFrameCahce = [],
+
+const updateCache = {},
+    logicFrameCahce = {},
+    logicFrameIndexArr = []
+
 
 export function initClient(element, io, url) {
 
@@ -65,7 +111,7 @@ export function initClient(element, io, url) {
     // TODO add container { playground, UIlayer}
     // TODO initUI()
 
-    let socket = io(url)
+    socket = io(url)
     // if (response) {// simulate network
     //     gameFrame = 0
 
@@ -73,22 +119,30 @@ export function initClient(element, io, url) {
     //         .load(setup)
     // }
 
-    socket.on('init_client', ({currentFrameIndex, fpsRadio, players}) => {
-        players = players
+    socket.on('init_client', ({ currentFrameIndex, fpsRadio, nextLogicFrame: initLogicFrame, players: playerList }) => {
+        players = playerList
         fpsRadio = fpsRadio
-        gameFrame = currentFrameIndex
+        gameFrame = currentFrameIndex * fpsRadio
+        nextGameFrame = gameFrame
+        logicFrameIndexArr.push(currentFrameIndex)
+        logicFrameCahce[currentFrameIndex] = initLogicFrame
         // cache frame
         // how to init nextGameFrame
         loader.add('adventurer', 'sheet/adventurer/adventurer.json')
             .load(setup)
+
+        Object.keys(players).forEach(id => {
+            initPlayer(players[id])
+        })
     })
 
-    socket.on('create_player', ({id, name, charactor, status}) => {
-        initPlayer({id, name, charactor, status})
+    socket.on('create_player', ({ id, name, charactor, status }) => {
+        console.log(id + ' : ' + name)
+        initPlayer({ id, name, charactor, status })
     })
 
-    socket.on('update_frame', (keys) => {
-        updateCache.push(keys)
+    socket.on('update_frame', (logicFrameIndex,keys) => {
+        updateCache[logicFrameIndex] = keys
     })
 
     function setup() {
@@ -105,52 +159,55 @@ export function initClient(element, io, url) {
             })
         }
 
-        state = play
+        state = cacheFrame
         app.ticker.add(delta => gameLoop(delta))
 
         // for test
+        // if(window.localStorage.getItem('AdventurerPlayer') !== null){
+        //     localPlayerId = window.localStorage.getItem('AdventurerPlayer')
+        // }else{
+        //     createPlayer('TEST', 'adventurer')
+        // }
         createPlayer('TEST', 'adventurer')
-        // let network = setInterval(communicate, 1000 / 60 * fpsRadio)
-        nextGameFrame = 0
 
-        logicFrameCahce.push(
-            {
-                'Nijia_001': {
-                    // position
-                    x: 0,
-                    y: 0,
-                    z: 0,
+        // logicFrameCahce.push(
+        //     {
+        //         'Nijia_001': {
+        //             // position
+        //             x: 0,
+        //             y: 0,
+        //             z: 0,
 
-                    // speed
-                    vx: 0,
-                    vy: 0,
-                    vz: 0,
-                    ax: 0,
-                    ay: 0,
-                    az: .6,
+        //             // speed
+        //             vx: 0,
+        //             vy: 0,
+        //             vz: 0,
+        //             ax: 0,
+        //             ay: 0,
+        //             az: .6,
 
-                    // action
-                    lastX: 'right',
-                    lastY: 'down',
-                    motion: 'right_idle',
-                    jumping: false,
-                    actionCounter: 0,
-                    attackCounter: 0,
-                    dashCounter: 0,
+        //             // action
+        //             lastX: 'right',
+        //             lastY: 'down',
+        //             motion: 'right_idle',
+        //             jumping: false,
+        //             actionCounter: 0,
+        //             attackCounter: 0,
+        //             dashCounter: 0,
 
-                    // ability
-                    speedDown: .4,
-                    actionCoolDown: 10,
-                    attackCoolDown: 20,
-                    dashCoolDown: 10,
+        //             // ability
+        //             speedDown: .4,
+        //             actionCoolDown: 10,
+        //             attackCoolDown: 20,
+        //             dashCoolDown: 10,
 
-                    //status
-                    HP: 100,
-                    maxHP: 100,
-                    MP: 60,
-                    maxMP: 60
-                }
-            })
+        //             //status
+        //             HP: 100,
+        //             maxHP: 100,
+        //             MP: 60,
+        //             maxMP: 60
+        //         }
+        //     })
     }
 }
 
@@ -158,60 +215,84 @@ function gameLoop(delta) {
     state(delta)
 }
 
-let counter
+function cacheFrame(delta) {
+    if (Object.keys(updateCache).length > defaultDelay) {
+        state = play
+        updateControlInterval = setInterval(updateControl, 1000 / 60 * fpsRadio)
+    }
+}
 
+// let counter
 function play(delta) {
     if (gameFrame > maxFrameCache) {
         gameFrame = 0
     } else {
         gameFrame += delta
     }
-
-    let index = Math.floor(delta / fpsRadio)
+    // let index = Math.floor(delta / fpsRadio)
     let offset = delta % fpsRadio
 
-    //
-    if (gameFrame > nextGameFrame) {
-        nextGameFrame += fpsRadio * (index + 1)
-        if(nextGameFrame > maxFrameCache){
-            nextGameFrame -= maxFrameCache
-        }
+    let curFrameIndex = nextGameFrame / fpsRadio,
+        nextFrameIndex = curFrameIndex + 1
 
-        if (updateCache.length > 0) {
+    // gameFrame count greater than nextGameFrame then do logic
+    if (gameFrame > nextGameFrame) {
+        console.log(updateCache)
+        console.log(curFrameIndex)
+        if (updateCache[nextFrameIndex] !== undefined) {
+
+            nextGameFrame += fpsRadio
+            if (nextGameFrame > maxFrameCache) {
+                nextGameFrame -= maxFrameCache
+            }
             // do some Logic
-            logicFrameCahce.push(
-                doSimpleLogic(logicFrameCahce[logicFrameCahce.length - 1], updateCache.shift())
-            )
+            logicFrameCahce[nextFrameIndex] =
+                doSimpleLogic(logicFrameCahce[curFrameIndex], updateCache[nextFrameIndex])
+
+            logicFrameIndexArr.push(curFrameIndex)
+        } else {
+            // send request get frameIndex operations
         }
     }
 
     // update game frame
-    if (logicFrameCahce.length > 0) {
-        if (index >= 0 && index >= logicFrameCahce.length) {
-            if (index >= logicFrameCahce.length) {
-                counter = fpsRadio
-                index = logicFrameCahce.length
-                updateGameFrame(logicFrameCahce[logicFrameCahce.length-1], offset)
-                return
-            } else {
-                counter = offset
+    if (logicFrameIndexArr.length > 0) {
+        logicFrameIndexArr.find((frameIndex, i, self) => {
+            if (frameIndex == curFrameIndex) {
+                updateDispalyFrame(logicFrameCahce[frameIndex], offset)
+                let pastFrame = self.splice(0, i)
+                pastFrame.map((pastFrameIndex) => {
+                    delete updateCache[pastFrameIndex]
+                    delete logicFrameCahce[pastFrameIndex]
+                })
+                return true
             }
-        } else {
-            counter += offset
-        }
+        })
+        // if (index >= 0 && index >= logicFrameCahce.length) {
+        //     if (index >= logicFrameCahce.length) {
+        //         counter = fpsRadio
+        //         index = logicFrameCahce.length
+        //         updateDispalyFrame(logicFrameCahce[logicFrameCahce.length-1], offset)
+        //         return
+        //     } else {
+        //         counter = offset
+        //     }
+        // } else {
+        //     counter += offset
+        // }
 
-        updateGameFrame(logicFrameCahce[index], offset)
+        // updateDispalyFrame(logicFrameCahce[index], offset)
 
-        if (counter == fpsRadio) {
-            logicFrameCahce.splice(0, index + 1)
-        } else if (index) {
-            logicFrameCahce.splice(0, index)
-        }
+        // if (counter == fpsRadio) {
+        //     logicFrameCahce.splice(0, index + 1)
+        // } else if (index) {
+        //     logicFrameCahce.splice(0, index)
+        // }
     }
 }
 
 // calculate interpolation rendering
-function updateGameFrame(logicFrame, offset) {
+function updateDispalyFrame(logicFrame, offset) {
     Object.keys(players).forEach(id => {
         let { x, y, z } = logicFrame[id]
 
@@ -232,8 +313,12 @@ function createPlayer(name, charactor, conf = controllConfig) {
         return
     }
 
-    socket.emit('create_player', {name, charactor})
-    socket.on('create_success', () => {
+    socket.emit('create_player', { name, charactor })
+    socket.on('create_success', (playerId) => {
+        //let initFrameIndex = logicFrameIndexArr[-1]
+        // logicFrameCahce[initFrameIndex][playerId] = initStatus
+        localPlayerId = playerId
+        logicFrameCahce[nextGameFrame/fpsRadio][playerId] = initStatus
         bindKey(conf)
     })
     // if (response) {// simulate network
@@ -279,9 +364,10 @@ function bindKey(conf) {
 
 
 // hanlder network request response 
-
 function updateControl() {
     // send control to server
+    socket.emit('control_event', localPlayerId, keys)
+    // console.log(localPlayerId + ' : ' +keys)
 }
 
 function communicate() {
